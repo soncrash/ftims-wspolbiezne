c0f470d326e77cbe6f40e051dbda937231223104
soncrash <soncrash@gmail.com>
2015-01-21 17:12:34 +0100

    Update Cw9.java

--- Cw9.java
+++ Cw9.java
@@ -2,11 +2,11 @@ package ftims.zad9;
 
 public class Cw9 {
 
-	private int iloscWatkow = 2;
-	private final int ROWS = 4, COLS = 4;
+	private int iloscWatkow = 16;
+	private final int ROWS = 1024, COLS = 1024;
 	private double[][] macierz = new double[ROWS][COLS+1];
-	
-
+	private long sum;
+	private long timeStart;
 	
 	public void uruchomWatki() throws InterruptedException{
 		Thread[] threads = new Thread[iloscWatkow];
@@ -18,17 +18,24 @@ public class Cw9 {
 			for(int c = 0; c < COLS +1 ; c++) {
 				macierz[r][c] /= d;
 			}
-			
+			timeStart = System.nanoTime();
 			for(int i = 0 ; i < iloscWatkow; i++) {
 				int start = i * (ROWS / iloscWatkow);
 				int end = (i + 1) * (ROWS / iloscWatkow);
 				w = new Jordan(start,end,macierz,r,COLS);
 		    	threads[i] = new Thread(w);
 		    	threads[i].start();
-		    	threads[i].join();
 			}
 			
+			for(int i = 0 ; i < iloscWatkow; i++) {
+				threads[i].join();
+			}
+			sum += (System.nanoTime() - timeStart);
 		}
+	}
+	
+	public long getSum() {
+		return sum;
 	}
 
 	public void pokazMacierz(double[][] matrix) {
@@ -49,11 +56,19 @@ public class Cw9 {
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
-		double[][] matrix = {{3,6,4,6,2},{4,4,3,1,5},{3,1,(5/4d),(5/3d),1},{(5/3d),(1/2d),9,5,3}};
+		double[][] matrix = new double[1024][1025];
+		for (int i = 0 ; i < 1024 ; i++) { 
+			for (int j = 0 ; j < 1025 ; j++) {
+				matrix[i][j] = Math.random();
+			}
+		}
 		Cw9 cw = new Cw9();
 		cw.setMatrix(matrix);
+		long timeStart = System.nanoTime();
 		cw.uruchomWatki();
-		cw.pokazMacierz(cw.getMatrix());
+		long timeStop = System.nanoTime();
+		System.out.println("Sekewncyjna część " + ((timeStop-timeStart) - cw.getSum()) + " [ms]");
+		System.out.println("Całość " + cw.getSum() + " [ms]") ;
 		
 	}
 }
